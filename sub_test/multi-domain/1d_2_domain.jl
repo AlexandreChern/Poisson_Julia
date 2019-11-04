9860fb5779f99c31e3407e4a31d7e02bc4490f87
Alexandre Chen <napoleonalexandre@gmail.com>
2019-11-04 10:04:49 -0800

    Finish 2-domain problem. Still need debugging

---
+++ sub_test/multi-domain/1d_2_domain.jl
@@ -0,0 +1,82 @@
+# Solving the Poisson Equation
+# We are solving the following Poisson Equation in 1D
+#
+# $$\left\{\begin{array}{rlrl}{u_{xx}} + \pi^2sin(\pi x) & {=0,} & {0\leq x \leq 1}  \\
+# {u_{0}} & {=0,} & {x=0} \\     Dirichlet Boundary Condition
+# {u}_x_{|1} & {=-\pi,} & {x=1}\end{array}\right.$$                  Neumann Boundary Condition
+
+# Using external Julia file
+
+include("diagonal_sbp.jl")
+
+using LinearAlgebra
+using SparseArrays
+function e(i,n)
+    A = Matrix{Float64}(I,n,n)
+    return A[:,i]
+end
+
+
+# Solve for special case h = 0.05, n = 20
+# Generating data
+
+
+h_list = [0.2, 0.1, 0.05, 0.025, 0.0125, 0.00625]
+
+# h_list = [0.02, 0.01, 0.005, 0.0025, 0.00125, 0.000625, 0.0003125] # uncomment to use for p = 4, 6, 8
+n_list = 1 ./h_list
+
+
+p = 2
+
+i = 3
+
+n = Integer(n_list[i])
+n_half = Integer(n/2)
+N = n + 1
+N_half = n_half + 1
+
+(D1, HI1, H1, r1) = diagonal_sbp_D1(p,n_half,xc=(0,0.5))
+(D2, S0, SN, HI2, H2, r2) = diagonal_sbp_D2(p,n_half,xc=(0,0.5))
+
+span = LinRange(0,1,N)
+#analy_sol = sin.(span*π)
+
+half_span_1 = LinRange(0,0.5,N_half)
+half_span_2 = LinRange(0.5,1,N_half)
+span = vcat(half_span_1,half_span_2)
+analy_sol = vcat(sin.(half_span_1*π),sin.(half_span_2*π))
+
+e0 = e(1,N_half);
+en = e(N_half,N_half);
+BS = SN - S0
+
+α = Dict(2=>1, 4=>0.2508560249, 6=>0.1878715026)
+γ = 2 # γ > 1 for stability
+σ₁ = -γ/(α[p]*h_list[i]) # For left boundary Dirichlet condition
+σ₂ = -γ/(α[p]*h_list[i]) # For right boundary Neumann condition
+β = 1
+ϵ = 1   # Interior
+
+g_L = 0
+g_R = -π
+
+A_u = D2 + β*HI1*BS'*e0*e0' + σ₁*HI1*e0*e0' + σ₁*HI1*en*en' + β*HI1*BS'*en*en' + ϵ*HI1*BS*en*en'
+
+A_v = D2 + σ₂*HI1*en*en'*D1 + σ₁*H1*e0*e0' + β*HI1*BS*e0*e0' + ϵ*HI1*BS*e0*e0'
+
+A1_v = - HI1 * σ₁*en*e0' - HI1*β*en*e0' - ϵ*HI1*BS*en*e0'
+
+A2_u = - HI1 * σ₁*e0*en' - HI1*β*e0*en' - ϵ*HI1*BS*e0*en'
+
+
+A = vcat(hcat(A_u,A1_v),hcat(A2_u, A_v))
+
+b1 = σ₁*HI1*g_L*e0 + β*HI1*BS'*g_L*e0 - π^2*sin.(half_span_1*π)
+b2 = σ₂*HI1*g_R*en - π^2*sin.(half_span_2*π)
+b = vcat(b1,b2)
+
+num_sol = A\b
+
+using Plots
+plot(span, num_sol)
