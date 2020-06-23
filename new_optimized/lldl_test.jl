dfd743c1058493cf245501db96dd241796a32b7a
Alexandre Chen <napoleonalexandre@gmail.com>
2020-06-22 20:31:52 -0700

    added test file for lldl

--- new_optimized/lldl_test.jl
+++ new_optimized/lldl_test.jl
@@ -1,3 +1,6 @@
+using LimitedLDLFactorizations
+using AMD, Metis
+
 A = [ 1.7     0     0     0     0     0     0     0   .13     0
         0    1.     0     0   .02     0     0     0     0   .01
         0     0   1.5     0     0     0     0     0     0     0
