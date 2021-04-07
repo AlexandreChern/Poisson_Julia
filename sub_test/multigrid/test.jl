e4509ac5b5485fb02ce9f57add6c69114a079111
Alexandre Chern <napoleonalexandre@gmail.com>
2021-04-07 16:43:28 -0700

    add modification

--- sub_test/multigrid/test.jl
+++ sub_test/multigrid/test.jl
@@ -1,11 +1,11 @@
 using SparseArrays
 using LinearAlgebra
 using Plots
-N = 64
+N = 63
 h = 1/N
 x = range(0,stop=1,length=N+1)
 x = x[2:end-1]
-u_exact = zeros(N-1)
+u_exact = zeros(N+1)
 
 
 function Jacobi(N,h,ω,k,nsteps)
@@ -35,14 +35,19 @@ function Jacobi(N,h,ω,k,nsteps)
     Rω = (1-ω) * sparse(I,N-1,N-1) + ω*RJ
 
     # v = randn(N-1)
-    J = 2:N
-    v = sin.(k*J/(N+1)*π)
+    J = 0:N
+    v = sin.(k*J/(N)*π)
 
     # nsteps = 1000
 
 
     for i in 1:nsteps
-        v .= Rω * v + ω.*D_inv*f*h^2
+        # v .= Rω * v + ω.*D_inv*f*h^2
+        v_new = copy(v)
+        for j = 2:N
+            v_new[j] = (1-ω) * v[j] + ω * 1/2 * (v[j-1] + v[j+1])
+        end
+        v = copy(v_new)
     end
 
     # @show v
@@ -51,14 +56,14 @@ function Jacobi(N,h,ω,k,nsteps)
     return norm(v-u_exact,Inf)
 end
 
-iters = 1:5:101
+iters = 1:2:101
 
 ω = 2/3
 err_1 = Jacobi.(N,h,ω,1,iters)
 err_3 = Jacobi.(N,h,ω,3,iters)
 err_6 = Jacobi.(N,h,ω,6,iters)
 
-fig1 = plot(iters,[err_1,err_3,err_6])
+fig1 = plot(iters,[err_1,err_3,err_6],ylims=[0,1])
 savefig(fig1,"jacobi")
 
 
@@ -84,23 +89,18 @@ function Gauss_Seidel(N,h,k,nsteps)
 
     RG = (D-L)\U
 
-    # ω = 0.5
-
-    # Rω = (1-ω) * sparse(I,N-1,N-1) + ω*RG
-
-    # v = randn(N-1)
-    J = 2:N
-    v = sin.(k*J/(N+1)*π)
+    J = 0:N
+    v = sin.(k*J/(N)*π)
 
     # nsteps = 1000
 
     for i in 1:nsteps
-        v .= RG * v + h^2 * σ * inv(Matrix(D-L)) * f
+        # v .= RG * v + h^2 * σ * inv(Matrix(D-L)) * f
+        for j = 2:N
+            v[j] = 1/2*(v[j-1] + v[j+1])
+        end
     end
 
-    # @show v
-    # @show norm(A*v)
-    # plot(x,v)
     return norm(v-u_exact,Inf)
 end
 
@@ -108,11 +108,11 @@ err_1 = Gauss_Seidel.(N,h,1,iters)
 err_3 = Gauss_Seidel.(N,h,3,iters)
 err_6 = Gauss_Seidel.(N,h,6,iters)
 
-fig1 = plot(iters,[err_1,err_3,err_6])
+fig1 = plot(iters,[err_1,err_3,err_6],ylims=[0,1])
 savefig(fig1,"gauss_seidel")
 
 
-function RB_Gauss_Seidel(N,h,ω,k,nsteps)
+function RB_Gauss_Seidel(N,h,k,nsteps)
     D = spzeros(N-1,N-1)
     L = spzeros(N-1,N-1)
     U = spzeros(N-1,N-1)
@@ -139,19 +139,22 @@ function RB_Gauss_Seidel(N,h,ω,k,nsteps)
     # Rω = (1-ω) * sparse(I,N-1,N-1) + ω*RG
 
     # v = randn(N-1)
-    J = 2:N
-    v = sin.(k*J/(N+1)*π)
+    J = 0:N
+    v = sin.(k*J/(N)*π)
 
     # nsteps = 1000
 
-
     for i in 1:nsteps
-        tmp = RG * v + h^2 * σ * inv(Matrix(D-L)) * f
-        for i in 1:N-1
-            if (norm(i,2) == 0)
-                v[i] .= 1/2 * (tmp[i-1] + tmp[i+1]) 
-            else
-                v[i] .= 1/2 * (tmp)
+        # v .= RG * v + h^2 * σ * inv(Matrix(D-L)) * f
+        for j = 2:N
+            if mod(j,2) == 0
+                v[j] = 1/2*(v[j-1] + v[j+1])
+            end
+        end
+        for j = 2:N
+            if mod(j,2) == 1
+                v[j] = 1/2*(v[j-1] + v[j+1])
+            end
         end
     end
 
@@ -160,3 +163,10 @@ function RB_Gauss_Seidel(N,h,ω,k,nsteps)
     # plot(x,v)
     return norm(v-u_exact,Inf)
 end
+
+err_1 = RB_Gauss_Seidel.(N,h,1,iters)
+err_3 = RB_Gauss_Seidel.(N,h,3,iters)
+err_6 = RB_Gauss_Seidel.(N,h,6,iters)
+
+fig1 = plot(iters,[err_1,err_3,err_6])
+savefig(fig1,"rg_gauss_seidel")
