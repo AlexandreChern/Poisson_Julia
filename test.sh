4a452648b38ef0876fa9ab35a3db19c44b484a72
Alexandre Chen <napoleonalexandre@gmail.com>
2021-06-21 16:56:21 -0700

    add Petsc file to load and solve

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for x in 8 16 32 64 128 256
+do
+	#x = $(( 2 ** $iter))
+	#x <<< 2^$iter
+	echo "N = " $x
+	time  ./loadsolve -fA data/A_$x.dat -fb data/b_$x.dat -ksp_rtol 1e-10  -ksp_converged_reason \
+		-ksp_type cg -pc_type mg
+done
+
