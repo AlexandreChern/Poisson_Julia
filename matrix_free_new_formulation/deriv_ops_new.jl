450ac4f40dc497e8a9ea2b8c8506205db2c31e80
Alexandre Chern <napoleonalexandre@gmail.com>
2021-07-13 18:15:02 -0700

    add GPU code

--- matrix_free_new_formulation/deriv_ops_new.jl
+++ matrix_free_new_formulation/deriv_ops_new.jl
@@ -1,4 +1,5 @@
 using SparseArrays
+using CUDA
 function D2_test(idata,odata,Nx,Ny,h)
     for i = 1:Nx
         for j = 1:Ny
@@ -161,6 +162,40 @@ function matrix_free_A_v5(idata,odata,Nx,Ny,h,alpha1,alpha2,alpha3,alpha4,beta)
             + abs(offset_x) * alpha4 * 2 * (idata[global_index] * h)  + coef_j[i] * beta * (idata[global_index+offset_j[i] * Ny]) ) / 2^(abs(offset_x) + abs(offset_y))
 
         end
+    end
+end
+
+
+function matrix_free_GPU(idata,odata,Nx,Ny,h,alpha1,alpha2,alpha3,alpha4,beta)
+    tidx = (blockIdx().x - 1) * TILE_DIM + threadIdx().x
+    tidy = (blockIdx().y - 1) * TILE_DIM + threadIdx().y
+
+    global_index = (tidx - 1) * Ny + tidy
+
+    alpha1 = alpha2 = alpha3 = alpha4 = beta = 1
+    odata .= 0
+    offset_j = spzeros(Int,Ny)
+    offset_j[2] = -1
+    offset_j[3] = -2
+    offset_j[Ny-1] = 1
+    offset_j[Ny-2] = 2
+    coef_j = spzeros(Ny)
+    coef_j[1] = 2* 1.5
+    coef_j[2] = - 2.0
+    coef_j[3] = 0.5
+    coef_j[Ny] = 2*1.5
+    coef_j[Ny-1] = -2.0
+    coef_j[Ny-2] = 0.5
+
+    if 1 <= global_index <= Nx*Ny
+        offset_x = div(2*Nx-tidx-3,Nx-2) - 1
+        offset_y = div(2*Ny-tidy-3,Ny-2) - 1
+        idata_index_x = (tidx-1)*Ny + tidy + (offset_x) * Nx
+        idata_index_y = (tidx-1)*Ny + tidy + offset_y
+
+        odata[global_index] = (( (idata[idata_index_x-Ny] - 2*idata[idata_index_x] + idata[idata_index_x + Ny])  + (idata[idata_index_y-1] - 2*idata[idata_index_y] + idata[idata_index_y + 1]) )   
+        + abs(offset_y) * 2 * (alpha1 * (1.5*idata[global_index]) - 2*idata[global_index+offset_y] + 0.5*idata[global_index+2*offset_y])
+        + abs(offset_x) * alpha4 * 2 * (idata[global_index] * h)  + coef_j[tidx] * beta * (idata[global_index+offset_j[tidx] * Ny]) ) / 2^(abs(offset_x) + abs(offset_y))      
     end
 
 end
