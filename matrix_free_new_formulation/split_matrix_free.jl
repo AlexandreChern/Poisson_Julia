159ab9114cb3106a62452c64453f834ddd877cf6
Alexandre Chen <napoleonalexandre@gmail.com>
2021-08-17 20:24:40 -0700

    add shared_memory and more efficient way of allocating memory

--- matrix_free_new_formulation/split_matrix_free.jl
+++ matrix_free_new_formulation/split_matrix_free.jl
@@ -4,15 +4,13 @@ using Random
 using Adapt
 using LinearAlgebra
 
-
-function D2_split(idata,odata,Nx,Ny,h,::Val{TILE_DIM1}, ::Val{TILE_DIM2}) where {TILE_DIM1, TILE_DIM2}
+function D2_split_naive(idata,odata,Nx,Ny,h,::Val{TILE_DIM1}, ::Val{TILE_DIM2}) where {TILE_DIM1, TILE_DIM2}
     tidx = threadIdx().x
     tidy = threadIdx().y
 
     i = (blockIdx().x - 1) * TILE_DIM1 + tidx
     j = (blockIdx().y - 1) * TILE_DIM2 + tidy
 
-    global_index = (i-1)*Ny+j
     @inbounds if 0 <= i <= Nx && 1 <= j <= Ny
         odata[i,j] = 0 # maybe do this on local memory
     end
@@ -24,6 +22,66 @@ function D2_split(idata,odata,Nx,Ny,h,::Val{TILE_DIM1}, ::Val{TILE_DIM2}) where 
     nothing
 end
 
+function D2_split(idata,odata,Nx,Ny,h,::Val{TILE_DIM1}, ::Val{TILE_DIM2}) where {TILE_DIM1, TILE_DIM2}
+    tidx = threadIdx().x
+    tidy = threadIdx().y
+
+    i = (blockIdx().x - 1) * TILE_DIM1 + tidx
+    j = (blockIdx().y - 1) * TILE_DIM2 + tidy
+
+    si = tidx + 1
+    sj = tidy + 1
+
+
+    HALO_WIDTH = 2 # For second order derivative
+
+    tile = @cuStaticSharedMem(eltype(idata), (TILE_DIM1+2, TILE_DIM2+2))
+
+    global_index = (i-1)*Ny+j
+    @inbounds if 0 <= i <= Nx && 1 <= j <= Ny
+        odata[i,j] = 0 # maybe do this on local memory
+    end
+
+
+    @inbounds if 1 <= i <= Nx && 1 <= j <= Ny
+        tile[si,sj] = idata[i,j]
+    end
+    sync_threads()
+
+    # if tidx == 1 && 1 <= j <= Ny
+    #     tile[si-1,sj] = tile[Nx+si-2,sj]
+    # #     tile[si+Nx,sj] = tile[si,sj]
+    # end
+
+    # sync_threads()
+
+    # if 1 <= i <= Nx && j <= 1
+    #     tile[si,sj-1] = tile[si,Ny+sj-2]
+    #     tile[si,sj+Ny] = tile[si,sj]
+    # end
+
+    sync_threads()
+
+    # @inbounds if 2 <= i <= Nx -1 && 2 <= j <= Ny - 1 &&  1 <= si <= TILE_DIM1 + 1 && 1 <= sj <= TILE_DIM2 + 1 
+    #     odata[i,j] = (tile[si-1,sj] + tile[si+1,sj] + tile[si,sj-1] + tile[si,sj+1] - 4*tile[si,sj])
+    # end 
+
+    nothing
+end
+
+
+struct boundary_containers
+    CPU_W
+    CPU_W_T
+    CPU_E
+    CPU_E_T
+    CPU_N
+    CPU_S
+    CPU_OUT_W
+    CPU_OUT_E
+    CPU_OUT_N
+    CPU_OUT_S
+end
 
 function matrix_free_A(idata,odata)
     # odata .= 0
@@ -34,18 +92,33 @@ function matrix_free_A(idata,odata)
     griddim = (div(Nx,TILE_DIM_1) + 1, div(Ny,TILE_DIM_2) + 1)
 	blockdim = (TILE_DIM_1,TILE_DIM_2)
 
-    CPU_W = zeros(Nx,3)
-    CPU_W_T = zeros(3,Nx)
-    CPU_E = zeros(Nx,3)
-    CPU_E_T = zeros(3,Nx)
-    CPU_N = zeros(3,Ny)
-    CPU_S = zeros(3,Ny)
+    # CPU_W = zeros(Nx,3)
+    # CPU_W_T = zeros(3,Nx)
+    # CPU_E = zeros(Nx,3)
+    # CPU_E_T = zeros(3,Nx)
+    # CPU_N = zeros(3,Ny)
+    # CPU_S = zeros(3,Ny)
 
 
-    CPU_OUT_W = zeros(Nx,3)
-    CPU_OUT_E = zeros(Nx,3)
-    CPU_OUT_N = zeros(3,Ny)
-    CPU_OUT_S = zeros(3,Ny)
+    # CPU_OUT_W = zeros(Nx,3)
+    # CPU_OUT_E = zeros(Nx,3)
+    # CPU_OUT_N = zeros(3,Ny)
+    # CPU_OUT_S = zeros(3,Ny)
+
+    CPU_W = Array{Float64,2}(undef,Nx,3)
+    CPU_W_T = Array{Float64,2}(undef,3,Nx)
+    CPU_E = Array{Float64,2}(undef,Nx,3)
+    CPU_E_T = Array{Float64,2}(undef,3,Nx)
+    CPU_N = Array{Float64,2}(undef,3,Ny)
+    CPU_S = Array{Float64,2}(undef,3,Ny)
+
+
+    
+
+    CPU_OUT_W = Array{Float64,2}(undef,Nx,3)
+    CPU_OUT_E = Array{Float64,2}(undef,Nx,3)
+    CPU_OUT_N = Array{Float64,2}(undef,3,Ny)
+    CPU_OUT_S = Array{Float64,2}(undef,3,Ny)
 
     # copyto!(CPU_W,idata[:,1:3])
     # copyto!(CPU_E,idata[:,end-2:end])
@@ -57,14 +130,16 @@ function matrix_free_A(idata,odata)
     copyto!(CPU_N,view(idata,1:3,:))
     copyto!(CPU_S,view(idata,Nx-2:Nx,:))
 
-    @cuda threads=blockdim blocks=griddim D2_split(idata,odata,Nx,Ny,h,Val(TILE_DIM_1), Val(TILE_DIM_2))
+    @cuda threads=blockdim blocks=griddim D2_split_naive(idata,odata,Nx,Ny,h,Val(TILE_DIM_1), Val(TILE_DIM_2))
 
     ## CPU Calculation
 
     CPU_W_T .= CPU_W'
     CPU_E_T .= CPU_E'
-    CPU_OUT_W_T = zeros(3,Ny)
-    CPU_OUT_E_T = zeros(3,Ny)
+    # CPU_OUT_W_T = zeros(3,Ny)
+    # CPU_OUT_E_T = zeros(3,Ny)
+    CPU_OUT_W_T = Array{Float64,2}(undef,3,Ny)
+    CPU_OUT_E_T = Array{Float64,2}(undef,3,Ny)
 
 
     alpha1 = alpha2 = -13/h
@@ -163,6 +238,7 @@ function matrix_free_A(idata,odata)
 end
 
 
+
 function test_matrix_free_A(level)
     Nx = Ny = 2^level + 1
     h = 1/(Nx-1)
@@ -178,6 +254,9 @@ function test_matrix_free_A(level)
     griddim = (div(Nx,TILE_DIM_1) + 1, div(Ny,TILE_DIM_2) + 1)
 	blockdim = (TILE_DIM_1,TILE_DIM_2)
     @cuda threads=blockdim blocks=griddim D2_split(idata,odata,Nx,Ny,h,Val(TILE_DIM_1), Val(TILE_DIM_2))
+    @cuda threads=blockdim blocks=griddim D2_split_naive(idata,odata,Nx,Ny,h,Val(TILE_DIM_1), Val(TILE_DIM_2))
+
+    boundary_data = boundary_containers(zeros(Nx,3),zeros(3,Nx),zeros(Nx,3),zeros(3,Nx),zeros(3,Ny),zeros(3,Ny),zeros(Nx,3),zeros(Nx,3),zeros(3,Ny),zeros(3,Ny))
     matrix_free_A(idata,odata)
 
 
@@ -191,6 +270,16 @@ function test_matrix_free_A(level)
     t_D2 = (time() - t_start_D2) * 1000 / iter_times
     @show t_D2
     # End evaluating D2
+
+      # Evaluating only D2_naive
+      t_start_D2_naive = time()
+      for _ in 1:iter_times
+          @cuda threads=blockdim blocks=griddim D2_split_naive(idata,odata,Nx,Ny,h,Val(TILE_DIM_1), Val(TILE_DIM_2))
+      end
+      synchronize()
+      t_D2_naive = (time() - t_start_D2_naive) * 1000 / iter_times
+      @show t_D2_naive
+      # End evaluating D2
 
     t_start_A = time()
     for _ in 1:iter_times
