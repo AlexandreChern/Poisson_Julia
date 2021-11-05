a641cbe19d5914c5472dff18fa370c501ca2efa6
Alexandre Chern <napoleonalexandre@gmail.com>
2021-11-05 12:22:01 -0700

    add more changes

--- matrix_free_new_formulation/level_2_multigrid.jl
+++ matrix_free_new_formulation/level_2_multigrid.jl
@@ -182,3 +182,230 @@ function Assembling_matrix(level)
 
     return (A,b,H_tilde,Nx,Ny)
 end
+
+
+
+function prolongation_matrix(N)
+    # SBP preserving
+    # N = 2^level + 1
+    odata = spzeros(2*N-1,N)
+    for i in 1:2*N-1
+        if i % 2 == 1
+            odata[i,div(i+1,2)] = 1
+        else
+            odata[i,div(i,2)] = 1/2
+            odata[i,div(i,2)+1] = 1/2
+        end
+    end
+    return odata
+end
+
+function restriction_matrix(N)
+    # SBP preserving
+    odata = spzeros(div(N+1,2),N)
+    odata[1,1] = 1/2
+    odata[1,2] = 1/2
+    odata[end,end-1] = 1/2
+    odata[end,end] = 1/2
+    for i in 2:div(N+1,2)-1
+        odata[i,2*i-2] = 1/4
+        odata[i,2*i-1] = 1/2
+        odata[i,2*i] = 1/4
+    end
+    return odata
+end
+
+function restriction_matrix_normal(N)
+    # SBP preserving
+    odata = spzeros(div(N+1,2),N)
+    odata[1,1] = 1/2
+    odata[1,2] = 1/4
+    odata[end,end-1] = 1/4
+    odata[end,end] = 1/2
+    for i in 2:div(N+1,2)-1
+        odata[i,2*i-2] = 1/4
+        odata[i,2*i-1] = 1/2
+        odata[i,2*i] = 1/4
+    end
+    return odata
+end
+
+function prolongation_2d(N)
+    prolongation_1d = prolongation_matrix(N)
+    prolongation_2d = kron(prolongation_1d,prolongation_1d)
+    return prolongation_2d
+end
+
+function restriction_2d(N)
+    restriction_1d = restriction_matrix_normal(N)
+    restriction_2d = kron(restriction_1d,restriction_1d)
+    return restriction_2d
+end
+
+function jacobi_iter!(x,A,b;maxiter=4)
+    k = 0
+    n = length(x)
+    iter_times = 0
+    # while norm(A*x-b) >= 1e-8
+    for _ in 1:maxiter
+        iter_times += 1
+        for i = 1:n
+            σ = 0
+            for j = 1:n
+                if j != i
+                    σ = σ + A[i,j]*x[j]
+                end
+            end
+            x[i] = (b[i] - σ)/A[i,i]
+        end
+    end
+    # (x,iter_times)
+end
+
+function Two_Grid_Correction(;level=9,nu=10,use_galerkin=true)
+    # level = 9
+    (A,b,H_tilde,Nx,Ny) = Assembling_matrix(level);
+    (A_p,b_p,H_tilde_p,Nx_p,Ny_p) = Assembling_matrix(level-1);
+
+    x = zeros(length(b));
+    jacobi!(x,A,b;maxiter=nu*2);
+    
+    println("Without coarse grid correction, norm(A*x-b) after $(nu*2) iterations: $(norm(A*x-b))")
+
+    x = zeros(length(b));
+    x_p = zeros(length(b_p));
+    e_fine = zeros(length(b));
+
+    jacobi!(x,A,b;maxiter=nu);
+    r = b - A*x;
+    # @show norm(r)
+    println("Without coarse grid correction, norm(A*x-b) after 10 iterations: $(norm(A*x-b))")
+
+    f = restriction_2d(Nx) * r;
+    A_coarse = restriction_2d(Nx) * A * prolongation_2d(Nx_p);
+    # jacobi!(x_p,A_coarse,f);
+    if use_galerkin
+        x_p = A_coarse \ f
+    else
+        x_p = A_p \ f
+    end 
+    # jacobi!(x_p,A_p,f);
+    e_1 = prolongation_2d(Nx_p) * x_p;
+    x = x + e_1;
+    println("After coarse grid correction, norm(A*x-b): $(norm(A*x-b))")
+    jacobi!(x,A,b;maxiter=nu);
+    println("After coarse grid correction, norm(A*x-b) after another 10 iterations: $(norm(A*x-b))")
+end
+
+
+A_matrices = Dict()
+
+function Two_level_multigrid(A_matrices;level=8,L=3,nu=10,NUM_V_CYCLES=1,use_galerkin=true)
+    # level = 8
+    (A,b,H_tilde,Nx,Ny) = Assembling_matrix(level);
+    (A_p,b_p,H_tilde_p,Nx_p,Ny_p) = Assembling_matrix(level-1);
+
+    # L = 3 # multigrid level
+    v_values = Dict(1=>zeros(Nx*Ny))
+    rhs_values = Dict(1 => b)
+
+    # @show A_matrices
+    @show isempty(A_matrices)
+    if isempty(A_matrices)
+        A_matrices[1] = A
+        N_values = Dict(1=>Nx)
+        # NUM_V_CYCLES = 2
+        x = zeros(length(b));
+        v_values[1] = x
+        for cycle_number in 1:NUM_V_CYCLES
+            @show cycle_number
+            # max_iter = 10
+            jacobi!(v_values[1],A,b;maxiter=nu*2);
+            r = b - A_matrices[1]*v_values[1];
+            f = restriction_2d(Nx) * r;
+            A_coarse = restriction_2d(Nx) * A_values[1] * prolongation_2d(Nx_p);
+            if use_galerkin
+                x_p = A_coarse \ f
+                v_values[2] = x_p
+            else
+                x_p = A_p \ f
+                v_values[2] = x_p
+            end 
+            println("Pass first part")
+            e_1 = prolongation_2d(Nx_p) * v_values[2];
+            v_values[1] = v_values[1] + e_1;
+            println("After coarse grid correction, norm(A*x-b): $(norm(A*v_values[1]-b))")
+            jacobi!(v_values[1],A_values[1],b;maxiter=nu);
+            # @show norm(A_matrices[1] * v_values[1] - b)
+        end
+        return (v_values[1],norm(A_matrices[1] * v_values[1] - b))
+    else
+        N_values = Dict(1=>Nx)
+        # NUM_V_CYCLES = 2
+        # x = zeros(length(b));
+        # v_values[1] = x
+        for cycle_number in 1:NUM_V_CYCLES
+            @show cycle_number
+            # max_iter = 10
+            jacobi!(v_values[1],A,b;maxiter=nu*2);
+            r = b - A*v_values[1];
+            f = restriction_2d(Nx) * r;
+            A_coarse = restriction_2d(Nx) * A * prolongation_2d(Nx_p);
+            if use_galerkin
+                x_p = A_coarse \ f
+                v_values[2] = x_p
+            else
+                x_p = A_p \ f
+                v_values[2] = x_p
+            end 
+            println("Pass first part")
+            e_1 = prolongation_2d(Nx_p) * v_values[2];
+            v_values[1] = v_values[1] + e_1;
+            println("After coarse grid correction, norm(A*x-b): $(norm(A*v_values[1]-b))")
+            jacobi!(v_values[1],A_matrices[1],b;maxiter=nu);
+            # @show norm(A_matrices[1] * v_values[1] - b)
+        end
+        return (v_values[1],norm(A_matrices[1] * v_values[1] - b))
+        # return (v_values[1],norm(A_matrices[1] * v_values[1] - b) / (N_values[1] - 1))
+    end
+end
+
+
+function mg_preconditioned_CG_v2(A,b,x;maxiter=length(b),abstol=sqrt(eps(real(eltype(b)))),NUM_V_CYCLES=1,nu=4,mg_level=mg_level,use_galerkin=true)
+    r = b - A * x;
+    rnew = similar(r)
+    # z = jacobi(A,r,maxiter=jacobi_iter)
+    # mg_level = 3
+    A_matrices = Dict()
+    z = mg_v2(A,r,A_matrices,mg_level,NUM_V_CYCLES=NUM_V_CYCLES,nu=nu,use_galerkin=use_galerkin)
+    znew = similar(z)
+    p = z;
+    Ap = A*p;
+    num_iter_steps = 0
+    norms = [norm(r)]
+    for step = 1:maxiter
+    # for _ in 1:40
+        num_iter_steps += 1
+        alpha = r'*z/(p'*Ap)
+        mul!(Ap,A,p);
+        alpha = r'*z / (p'*Ap)
+        x .= x .+ alpha * p;
+        rnew .= r .- alpha * Ap;
+        rsnew = rnew' * rnew
+        append!(norms,sqrt(rsnew))
+        if sqrt(rsnew) < abstol
+              break
+        end
+        # p = r + (rsnew / rsold) * p;
+        # znew = jacobi(A,rnew,maxiter=jacobi_iter=10);
+        znew = mg_v2(A,rnew,A_matrices,mg_level,NUM_V_CYCLES=NUM_V_CYCLES,nu=nu,use_galerkin=use_galerkin)
+        beta = rnew'*znew/(r'*z);
+        p .= znew .+ beta * p;
+        z .= znew
+        r .= rnew
+        # @show rsnew
+        # @show step, rsnew
+    end
+    # @show num_iter_steps
+    return num_iter_steps, norms
+end
