5bc17b48095ea1e16f56f7abff685d88b1239be4
Alexandre Chen <napoleonalexandre@gmail.com>
2021-11-18 19:15:40 -0800

    improved performance

--- matrix_free_new_formulation/level_2_multigrid_new.jl
+++ matrix_free_new_formulation/level_2_multigrid_new.jl
@@ -311,54 +311,7 @@ function regularCG!(A,b,x,H1,exact;maxiter=length(b)^2,abstol=sqrt(eps(real(elty
     return E, num_iter_steps, norms
 end
 
-function MGCG!(A,b,x,H1,exact, my_solver;smooth_steps = 4,maxiter=length(b)^2,abstol=sqrt(eps(real(eltype(b)))))
-    r = b - A * x;
-    z = zeros(length(b))
-    (M, R, H, I_p, A_2h, I_r, IN) = precond_matrix(A,b;m=smooth_steps,solver="jacobi")
-    # Two_level_multigrid!(A,r,z;nu=smooth_steps, solver = my_solver)
-    z = M*r #test 
-    p = z;
-    rzold = r'*z
-    
-    num_iter_steps = 0
-    norms = [sqrt(r'*H1*r)]
- 
-    diff = x-exact
-    err = sqrt(diff'*A*diff)
-    
-    E = [err]
-    for step = 1:maxiter
-        Ap = A*p;
-        num_iter_steps += 1
-        alpha = rzold/(p'*Ap)
-        x .= x .+ alpha * p;
 
-        diff = x-exact
-        err = sqrt(diff'*A*diff)
-        append!(E, err)
-        
-        r .= r .- alpha * Ap;
-      
-        norm_r = sqrt(r' * r)
-        append!(norms,norm_r)
-
-        if norm_r < abstol
-            break
-        end
-
-    
-        z .= 0
-        # Two_level_multigrid!(A,r,z;nu=smooth_steps, solver = my_solver)
-        z = M*r
-
-        rznew = r' * z
-        beta = rznew/rzold;
-        p = z + beta * p;
-        rzold = rznew
-    end
-
-    return E, num_iter_steps, norms
-end
 
 function jacobi_brittany!(x,A,b;maxiter=3, ω = 2/3)
 
@@ -460,33 +413,102 @@ function mg_preconditioned_CG(A,b,x;maxiter=length(b),abstol=sqrt(eps(real(eltyp
         append!(errors,sqrt(direct_sol' * A * direct_sol))
     end
 
-    for step = 1:maxiter
+    rzold = r'*z
+
+    # for step = 1:maxiter
+    for step = 1:5
         num_iter_steps += 1
-        alpha = r'*z/(p'*A*p)
+        @show norm(A*p)
+
+        # alpha = r'*z/(p'*A*p)
+        alpha = rzold / (p'*A*p)
+        @show alpha
+
         x .= x .+ alpha * p;
-        rnew .= r .- alpha * A*p;
-        rsnew = rnew' * rnew
-        append!(norms,sqrt(rsnew))
+
+        # rnew .= r .- alpha * A*p;
+        r .= r .- alpha * A*p
+        rs = r' * r
+        append!(norms,sqrt(rs))
         if direct_sol != 0 && H_tilde != 0
             error = sqrt((x - direct_sol)' * A * (x - direct_sol))
+            @show error
             append!(errors,error)
         end
-        if sqrt(rsnew) < abstol
+        if sqrt(rs) < abstol
             break
         end
         # p = r + (rsnew / rsold) * p;
         # znew = jacobi(A,rnew,maxiter=jacobi_iter=10);
         # znew = Two_level_multigrid(A,rnew;nu=nu,NUM_V_CYCLES=1)[1]
-        znew = M*rnew
-        beta = rnew'*znew/(r'*z);
-        p .= znew .+ beta * p;
-        z .= znew
-        r .= rnew
+        # znew .= M*rnew
+        z = M*r
+        rznew = r'*z
+        beta = rznew/(rzold);
+        @show beta
+        p = z + beta * p;
+        # z .= znew
+        # r .= rnew
         # @show rsnew
         # @show step, rsnew
+        rzold = rznew
     end
     # @show num_iter_steps
     return num_iter_steps, norms, errors
+end
+
+function MGCG!(A,b,x,H1,exact, my_solver;smooth_steps = 4,maxiter=length(b)^2,abstol=sqrt(eps(real(eltype(b)))))
+    r = b - A * x;
+    z = zeros(length(b))
+    (M, R, H, I_p, A_2h, I_r, IN) = precond_matrix(A,b;m=smooth_steps,solver="jacobi")
+    # Two_level_multigrid!(A,r,z;nu=smooth_steps, solver = my_solver)
+    z = M*r #test 
+    p = z;
+    rzold = r'*z
+    
+    num_iter_steps = 0
+    norms = [sqrt(r'*H1*r)]
+ 
+    diff = x-exact
+    err = sqrt(diff'*A*diff)
+    
+    E = [err]
+    # for step = 1:maxiter
+    for step = 1:5
+        Ap = A*p;
+        @show norm(A*p)
+        num_iter_steps += 1
+        alpha = rzold/(p'*Ap)
+        @show alpha
+        x .= x .+ alpha * p;
+
+        diff = x-exact
+        err = sqrt(diff'*A*diff)
+        @show err
+        append!(E, err)
+        
+        r .= r .- alpha * Ap;
+      
+        norm_r = sqrt(r' * r)
+        append!(norms,norm_r)
+
+        if norm_r < abstol
+            break
+        end
+
+    
+        z .= 0
+        # Two_level_multigrid!(A,r,z;nu=smooth_steps, solver = my_solver)
+        z = M*r
+
+        rznew = r' * z
+        beta = rznew/rzold;
+        @show beta
+        p = z + beta * p;
+        rzold = rznew
+    end
+
+    return E, num_iter_steps, norms
 end
 
 function test_preconditioned_CG(;level=6,nu=3,ω=2/3)
@@ -521,7 +543,7 @@ function test_preconditioned_CG(;level=6,nu=3,ω=2/3)
     my_solver = "jacobi"
     x0 = zeros(Nx*Ny)
     (E_mgcg, num_iter_steps_mgcg, norms_mgcg) = MGCG!(A,b,x0,H_tilde,direct_sol,my_solver;smooth_steps = nu,maxiter=20000,abstol=abstol)
-    
+
 
     # plot(error_mg_cg,label="error_mg_cg")
     # plot!(error_mg_cg_bound,label="error_mg_cg_bound")
