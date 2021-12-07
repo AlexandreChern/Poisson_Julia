921ea0230d43d51c6093ed98dce445904f10737b
Alexandre Chern <napoleonalexandre@gmail.com>
2021-12-06 16:07:30 -0800

    some minor change to test function

--- variable_coefficients/poisson_variable_coef.jl
+++ variable_coefficients/poisson_variable_coef.jl
@@ -1,7 +1,6 @@
 using LinearAlgebra
 using SparseArrays
 using Plots
-# include("../matrix_free_new_formulation/diagonal_sbp.jl")
 include("metrics.jl")
 
 # Solving Poisson Equation with Coordinate Transformation
@@ -14,6 +13,7 @@ Lx = 80
 Ly = 80
 
 (kx,ky) = (π/Lx, π/Ly)
+
 
 vex(x,y,e) = begin
     if e == 1
@@ -82,101 +82,112 @@ RSH2 = 18
 μshear = cs^2 * ρ
 η = μshear / (2 * cs)
 
-N = 2^6
-δNp = N + 1
-SBPp   = 2
+let 
+    errors = []
+    # for level in [6,7,8,9,10]
+    for level in [6,7,8,9]
+        N = 2^level
+        δNp = N + 1
+        SBPp = 6
 
 
 
-function u(x,y)
-    return sin.(π*x/Lx .+ π*y/Ly)
+
+        # const BC_DIRICHLET = 1
+        # const BC_NEUMANN = 2
+        # const BC_LOCKED_INTERFACE = 0
+        # const BC_JUMP_INTERFACE = 7
+
+        bc_map = [BC_DIRICHLET, BC_DIRICHLET, BC_NEUMANN, BC_NEUMANN,
+        BC_JUMP_INTERFACE]
+
+        nelems = 1
+        nfaces = 4
+
+        EToN0 = zeros(Int64, 2, nelems)
+        EToN0[1, :] .= N
+        EToN0[2, :] .= N
+
+
+
+        Nr = EToN0[1, :][1]
+        Ns = EToN0[2, :][1]
+
+        
+
+        # xf = (r,s) -> (r,ones(size(r)),zeros(size(r)))
+        # yf = (r,s) -> (s,zeros(size(s)),ones(size(s)))
+        # metrics = create_metrics(p,Nr,Ns)
+
+        el_x = 10
+        el_y = 10
+        xt = (r,s) -> (el_x .* tan.(atan((Lx )/el_x).* (0.5*r .+ 0.5))  , el_x .* sec.(atan((Lx )/el_x).* (0.5*r .+ 0.5)).^2 * atan((Lx)/el_x) * 0.5 ,zeros(size(s)))
+        yt = (r,s) -> (el_y .* tan.(atan((Ly )/el_y).* (0.5*s .+ 0.5))  , zeros(size(r)), el_y .* sec.(atan((Ly )/el_y).*(0.5*s .+ 0.5)) .^2 * atan((Ly )/el_y) * 0.5 )
+
+        metrics = create_metrics(SBPp,Nr,Ns,xt,yt)
+
+        Nrp = Nr + 1
+        Nsp = Ns + 1
+        Np = Nrp + Nsp
+
+        # r = range(-1,stop=1,length=Nrp)
+        # s = range(-1,stop=1,length=Nsp)
+
+        # r = ones(1,Nsp) ⊗ r
+        # s = s' ⊗ ones(Nrp)
+
+        # (x,xr,xs) = xf(r,s)
+        # (y,yr,ys) = yf(r,s)
+
+        # J = xr .* ys - xs .* yr
+        # @assert minimum(J) > 0
+
+        LFtoB = [BC_DIRICHLET,BC_DIRICHLET,BC_NEUMANN,BC_NEUMANN]
+        OPTYPE = typeof(locoperator(2,8,8))
+
+        e = 1
+        lop = Dict{Int64,OPTYPE}()
+        lop[e] = locoperator(SBPp,Nr,Ns,metrics,LFtoB)
+
+        factorization = (x) -> cholesky(Symmetric(x))
+        M = SBPLocalOperator1(lop,Nr,Ns,factorization)
+
+        x_coord = metrics.coord[1]
+        y_coord = metrics.coord[2]
+        ge = zeros(Nrp*Nsp)
+        gδe = zeros(Nrp*Nsp)
+        δ = zeros(Nrp*Nsp)
+        # ge = -2π^2 * u.(x_coord,y_coord')[:]
+
+
+        bc_Dirichlet = (lf,x,y,e,δ) -> vex(x,y,e) # needs to be changed
+        bc_Neumann   = (lf,x,y,nx,ny,e,δ) -> (nx .* vex_x(x,y,e) + ny .* vex_y(x,y,e)) # needs to be changed
+        in_jump = (lf,x,y,e) -> zeros(Nrp*Nsp)
+        # locbcarray_mod!(ge,lop[e],LFtoB,bc_Dirichlet,bc_Neumann,(e))
+
+        locbcarray!(ge,gδe,lop[e],LFtoB,bc_Dirichlet,bc_Neumann,in_jump,(e, δ))
+
+        source = (x,y,e) -> (-vex_xx(x,y,e)-vex_yy(x,y,e))
+        locsourcearray!(ge,source,lop[e],e)
+
+        direct_sol = M.F[e] \ ge
+        direct_sol_reshaped = reshape(direct_sol,Nrp,Nsp)
+
+        xseries = x_coord[:,1]
+        yseries = y_coord[1,:]
+        # plot(xseries,yseries,direct_sol_reshaped,st=:surface)
+
+        # plot(xseries,yseries,direct_sol_reshaped,st=:surface,camera=(45,45))
+
+        analy_sol = vex(x_coord,y_coord,e)[:]
+        numerical_error = sqrt((direct_sol - analy_sol)' * lop[e].JH * (direct_sol - analy_sol))
+        append!(errors,numerical_error)
+    end
+
+    log_errors = log2.(errors)
+    println(log_errors)
+
+    for i in 2:length(errors)
+        println(log_errors[i-1] - log_errors[i])
+    end
 end
-
-
-# const BC_DIRICHLET = 1
-# const BC_NEUMANN = 2
-# const BC_LOCKED_INTERFACE = 0
-# const BC_JUMP_INTERFACE = 7
-
-bc_map = [BC_DIRICHLET, BC_DIRICHLET, BC_NEUMANN, BC_NEUMANN,
-BC_JUMP_INTERFACE]
-
-nelems = 1
-nfaces = 4
-
-EToN0 = zeros(Int64, 2, nelems)
-EToN0[1, :] .= N
-EToN0[2, :] .= N
-
-
-
-Nr = EToN0[1, :][1]
-Ns = EToN0[2, :][1]
-
-p = 2
-
-# xf = (r,s) -> (r,ones(size(r)),zeros(size(r)))
-# yf = (r,s) -> (s,zeros(size(s)),ones(size(s)))
-# metrics = create_metrics(p,Nr,Ns)
-
-el_x = 10
-el_y = 10
-xt = (r,s) -> (el_x .* tan.(atan((Lx )/el_x).* (0.5*r .+ 0.5))  , el_x .* sec.(atan((Lx )/el_x).* (0.5*r .+ 0.5)).^2 * atan((Lx)/el_x) * 0.5 ,zeros(size(s)))
-yt = (r,s) -> (el_y .* tan.(atan((Ly )/el_y).* (0.5*s .+ 0.5))  , zeros(size(r)), el_y .* sec.(atan((Ly )/el_y).*(0.5*s .+ 0.5)) .^2 * atan((Ly )/el_y) * 0.5 )
-
-metrics = create_metrics(p,Nr,Ns,xt,yt)
-
-Nrp = Nr + 1
-Nsp = Ns + 1
-Np = Nrp + Nsp
-
-# r = range(-1,stop=1,length=Nrp)
-# s = range(-1,stop=1,length=Nsp)
-
-# r = ones(1,Nsp) ⊗ r
-# s = s' ⊗ ones(Nrp)
-
-# (x,xr,xs) = xf(r,s)
-# (y,yr,ys) = yf(r,s)
-
-# J = xr .* ys - xs .* yr
-# @assert minimum(J) > 0
-
-LFtoB = [BC_DIRICHLET,BC_DIRICHLET,BC_NEUMANN,BC_NEUMANN]
-OPTYPE = typeof(locoperator(2,8,8))
-
-e = 1
-lop = Dict{Int64,OPTYPE}()
-lop[e] = locoperator(SBPp,Nr,Ns,metrics,LFtoB)
-
-factorization = (x) -> cholesky(Symmetric(x))
-M = SBPLocalOperator1(lop,Nr,Ns,factorization)
-
-x_coord = metrics.coord[1]
-y_coord = metrics.coord[2]
-ge = zeros(Nrp*Nsp)
-gδe = zeros(Nrp*Nsp)
-δ = zeros(Nrp*Nsp)
-# ge = -2π^2 * u.(x_coord,y_coord')[:]
-
-
-bc_Dirichlet = (lf,x,y,e,δ) -> vex(x,y,e) # needs to be changed
-bc_Neumann   = (lf,x,y,nx,ny,e,δ) -> (nx .* vex_x(x,y,e) + ny .* vex_y(x,y,e)) # needs to be changed
-in_jump = (lf,x,y,e) -> zeros(Nrp*Nsp)
-# locbcarray_mod!(ge,lop[e],LFtoB,bc_Dirichlet,bc_Neumann,(e))
-
-locbcarray!(ge,gδe,lop[e],LFtoB,bc_Dirichlet,bc_Neumann,in_jump,(e, δ))
-
-source = (x,y,e) -> (-vex_xx(x,y,e)-vex_yy(x,y,e))
-locsourcearray!(ge,source,lop[e],e)
-
-direct_sol = M.F[e] \ ge
-direct_sol_reshaped = reshape(direct_sol,Nrp,Nsp)
-
-xseries = x_coord[:,1]
-yseries = y_coord[1,:]
-plot(xseries,yseries,direct_sol_reshaped,st=:surface)
-
-plot(xseries,yseries,direct_sol_reshaped,st=:surface,camera=(45,45))
-
-error = sqrt((direct_sol - analy_sol)' * lop[e].JH * (direct_sol - analy_sol))
