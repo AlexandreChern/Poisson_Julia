78f8a5b8fe6c29cc94a10efd581de046a940def1
Alexandre Chern <napoleonalexandre@gmail.com>
2022-02-18 16:07:40 -0800

    minor change

--- matrix_free_new_formulation/p_equals_4/test_matrix_free.jl
+++ matrix_free_new_formulation/p_equals_4/test_matrix_free.jl
@@ -3,7 +3,7 @@ include("matrix-free-p4.jl")
 include("matrix-free-p2.jl")
 
 
-level = 13
+level = 11
 i = j = level
 
 h_list_x = [1/2^1, 1/2^2, 1/2^3, 1/2^4, 1/2^5, 1/2^6, 1/2^7, 1/2^8, 1/2^9, 1/2^10, 1/2^11, 1/2^12, 1/2^13, 1/2^14]
@@ -68,7 +68,7 @@ matrix_free_N(idata,odata,Nx,Ny,hx,hy)
 
 ## Performance benchmarking
 
-repetitions = 100
+repetitions = 1000
 # time_D2 = @elapsed for _ in 1:repetitions
 #     odata_GPU .= 0
 #     D2_matrix_free_p2(idata_GPU,odata_GPU)
